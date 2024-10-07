458dba7966ab31e606b2da81d260be3ae7f70952
Dor Amar <doramar5555@gmail.com>
2024-10-07 14:13:50 +0530

    New

--- automated-deployment/app/app.py
+++ automated-deployment/app/app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/status')
 def status():
-    return jsonify(status="OK", message="API is running smoothly")
+    return jsonify(status="OK", message="API is running smoothly +++")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
